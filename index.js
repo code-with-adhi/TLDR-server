9d7e4eca5cc2adf7d1cb4e1a707083fc57b2f09e
code-with-adhi <125806079+code-with-adhi@users.noreply.github.com>
2025-08-11 11:17:34 +0530

    cors defined

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ dotenv.config();
 const app = express();
 const PORT = 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"], // whitelist
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const GNEWS_API_KEY = process.env.GNEWS_API_KEY;
